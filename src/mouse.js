41aafe35be9609ca5dad57bfcb6ab266fa3dbadd
Lucas Dower <lucasjdower@gmail.com>
2021-04-28 18:02:45 +0100

    More cleanup

--- src/mouse.js
+++ src/mouse.js
@@ -1,39 +1,40 @@
 const gl = document.querySelector("#c").getContext("webgl");
 
-var currentMouse = { x: -1, y: -1 };
-var previousMouse = { x: -1, y: -1 };
-var mouseButtons;
+var currentMouse =  { x: -1, y: -1, buttons: 0 };
+var previousMouse = { x: -1, y: -1, buttons: 0 };
 
 const MOUSE_LEFT = 1;
 const MOUSE_RIGHT = 2;
 
 module.exports = {
 
-    handleInput: function (e) {
+    handleInput: (e) => {
         previousMouse = currentMouse;
-        currentMouse = { x: e.clientX, y: e.clientY };
-        mouseButtons = e.buttons;
+        currentMouse = { x: e.clientX, y: e.clientY, buttons: e.buttons };
     },
 
-    isMouseLeftDown: function () {
-        return mouseButtons & MOUSE_LEFT;
+    isMouseLeftDown: () => {
+        return currentMouse.buttons & MOUSE_LEFT;
     },
 
-    isMouseRightDown: function () {
-        return mouseButtons & MOUSE_RIGHT;
+    isMouseRightDown: () => {
+        return currentMouse.buttons & MOUSE_RIGHT;
     },
 
-    getMouseDelta: function () {
-        return { dx: currentMouse.x - previousMouse.x, dy:  -(currentMouse.y - previousMouse.y) };
+    getMouseDelta: () => {
+        return {
+            dx:    currentMouse.x - previousMouse.x,
+            dy:  -(currentMouse.y - previousMouse.y)
+        };
     },
 
-    getMousePosNorm: function () {
+    getMousePosNorm: () => {
         let normX = 2 * (currentMouse.x / gl.canvas.width) - 1;
         let normY = -(2 * (currentMouse.y / gl.canvas.height) - 1);
         return { x: normX, y: normY };
     },
 
-    getMousePos: function () {
+    getMousePos: () => {
         return currentMouse;
     }
 
--- src/shaders.js
+++ src/shaders.js
@@ -16,28 +16,6 @@ const shadedProgram = twgl.createProgramInfo(gl, [shaded_vertex_shader, shaded_f
 const unshadedProgram = twgl.createProgramInfo(gl, [unshaded_vertex_shader, unshaded_fragment_shader]);
 const testProgram = twgl.createProgramInfo(gl, [test_vertex_shader, test_fragment_shader]);
 
-
-class Shader {
-
-    constructor(vertexPath, fragmentPath) {
-        const vertex_source = fs.readFileSync(vertexPath, 'utf8');
-        const fragment_source = fs.readFileSync(fragmentPath, 'utf8');
-        this.program = twgl.createProgramInfo(gl, [vertex_source, fragment_source]);
-        this.uniforms = {};
-    }
-
-    enable() {
-        gl.useProgram(this.program);
-    }
-
-    setUniform(uniform, value) {
-        this.enable();
-        this.uniforms.uniform = value;
-    }
-
-}
-
-
 module.exports.shadedProgram = shadedProgram;
 module.exports.unshadedProgram = unshadedProgram;
 module.exports.testProgram = testProgram;
